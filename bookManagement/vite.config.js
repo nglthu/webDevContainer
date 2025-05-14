83f9402b5c2ce36cdbd2ad0b100febcefbffcaee
Le Thu Nguyen <10757059+nglthu@users.noreply.github.com>
2025-05-14 16:38:28 +0000

    breeze test

--- bookManagement/vite.config.js
+++ bookManagement/vite.config.js
@@ -8,11 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-     server: {
-        hmr: {
-            host: process.env.CODESPACE_NAME ? process.env.CODESPACE_NAME + '-8000.app.github.dev' : null,
-            clientPort: process.env.CODESPACE_NAME ? 443 : null,
-            protocol: process.env.CODESPACE_NAME ? 'wss' : null
-        },
-    }
 });
