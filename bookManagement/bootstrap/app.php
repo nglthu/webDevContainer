36c21882daf2cd2818a7066a4e373dfa45d1b54c
lethunguyen <165644902+lethunguyen@users.noreply.github.com>
2025-05-28 14:32:26 +0000

    Disable CSRF protection for specific routes

--- bookManagement/bootstrap/app.php
+++ bookManagement/bootstrap/app.php
@@ -25,6 +25,9 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     })
      
+    ->withMiddleware(function (Middleware $middleware) { 
+        // Disable CSRF protection for specific routes
+        $middleware->validateCsrfTokens(except: [ '*', ]); })
 
     ->withExceptions(function (Exceptions $exceptions) {
         //
